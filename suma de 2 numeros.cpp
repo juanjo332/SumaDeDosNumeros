97544ed847499800e2ba0ae1dc32c7794dd339f3
juan <juanjo_070193@hotmail.com>
2013-02-10 10:58:55 -0600

    juan jose

---
+++ suma de 2 numeros.cpp
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int a, b,c;
+	
+	printf("Dame el primer numero ");
+	scanf("%d",&a);
+	printf("Dame el segundo numero ");
+	scanf("%d",&b);
+	c=a+b;
+	system("cls");
+	printf("El resultado de la suma es:%d",c);
+	
+	getchar();
+	return 0;
+	
+}
